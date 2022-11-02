17e6a2d3f12b05e527e072dbb06a847e945eefd5
Iman Sjamhudi <indoomni@gmail.com>
2022-11-02 11:21:22 +0700

    Misc.

--- server/register.js
+++ server/register.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const util = require('util');
+
 // console.log('Firebase admin register..');
 
-module.exports = ({ strapi }) => {};
+module.exports = ({ strapi }) => {
+  if (!strapi.inspect)
+    strapi.inspect = object =>
+      util.inspect(object, {
+        showHidden: false,
+        depth: null,
+        colors: true,
+      });
+};
